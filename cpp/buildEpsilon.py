2725f82612da17149eb400657e7b49a619f7d7e0
Randrian <Randrian@localhost>
2019-05-10 10:26:35 +0200

    * buildEpsilon: speedup for building the lookup table

--- cpp/buildEpsilon.py
+++ cpp/buildEpsilon.py
@@ -5,9 +5,9 @@ def saveEpsilon(epsilon, fname, CFG):
     """
     Save the material function to a file, e.g. for further plotting.
     """
-    imax = np.ceil((CFG["EPSMAX"] + 1.0) / CFG["EPSSTEP"])
+    imax = int(np.ceil((CFG["EPSMAX"] + 1.0) / CFG["EPSSTEP"]))
 
-    with open(fname) as fp:
+    with open(fname, "w") as fp:
         for i in range(imax):
             lambd = (i * CFG["EPSSTEP"]) - 1.0
 
@@ -18,67 +18,53 @@ def buildEpsilon(k1, ds0, s1, ds1, CFG):
     """
     Build a lookup table for the material function.
     """
-    print("EPSILON PARAMETERS", k1, ds0, s1, ds1)
+    print("EPSILON PARAMETERS", k1, ds0, s1, ds1, CFG["EPSSTEP"])
 
     # calculate the number of steps (+1.0 because EPSMIN is defined as -1)
     # TODO define EPSMIN in the config?
+    epsmin = -1.0
+    epsmax = CFG["EPSMAX"]
+    epsstep = CFG["EPSSTEP"]
+
+    def indexToLambda(index):
+        return index * epsstep + epsmin
+
+    def lambdaToIndex(lambd):
+        return int(np.ceil((lambd - epsmin) / epsstep))
+
     # epsilon cannot be smaller than -1 because for lambda = -1 the fiber already has 0 length
-    imax = int(np.ceil((CFG["EPSMAX"] + 1.0) / CFG["EPSSTEP"]))
+    imax = lambdaToIndex(epsmax)#int(np.ceil((epsmax - epsmin) / epsstep))
 
-    # initialize the lists
-    epsilon = np.zeros(imax)
-    epsbar = np.zeros(imax)
-    epsbarbar = np.zeros(imax)
+    lambd = indexToLambda(np.arange(imax))
 
-    # iterate over all steps
-    for i in range(imax):
-        # calculate the current argument (-1 because we start with EPSMIN = -1)
-        lambd = (i * CFG["EPSSTEP"]) - 1.0
+    # define epsbarbar according to p 43 eq 4.1.5
 
-        epsbarbar[i] = 0
+    # the linear spring regime (0 < lambda < s1)
+    epsbarbar = np.ones(imax) * k1
 
-        # define epsbarbar according to p 43 eq 4.1.5
-        if lambd > 0:
-            # constant
-            epsbarbar[i] += k1
+    # buckling for compression
+    if ds0 != 0.0:
+        buckling = lambd < 0
+        epsbarbar[buckling] = k1 * np.exp(lambd[buckling] / ds0)
+    # and exponential strech for overstreching fibers
+    if ds1 > 0.0:
+        streching = lambd > s1
+        epsbarbar[streching] = k1 * np.exp((lambd[streching] - s1) / ds1)
 
-            # of larger than s1, it grows exponentially
-            if lambd > s1 and ds1 > 0.0:
-                epsbarbar[i] += k1 * (np.exp((lambd - s1) / ds1) - 1.0)
-        else:
-            # for negative values it decays exponentially
-            if ds0 != 0.0:
-                epsbarbar[i] += k1 * np.exp(lambd / ds0)
-            else:
-                epsbarbar[i] += k1
-
-        # savety to prevent overflow
-        if epsbarbar[i] > 10e10:
-            epsbarbar[i] = 10e10
+    # savety to prevent overflow
+    epsbarbar[epsbarbar > 10e10] = 10e10
 
     # epsbar is the integral over epsbarbar
-    # TODO perhaps we can do this with cumsum
-    sum = 0.0
-    for i in range(imax):
-        sum += epsbarbar[i] * CFG["EPSSTEP"]
-        epsbar[i] = sum
+    epsbar = np.cumsum(epsbarbar * epsstep)
 
     # define the offset so that for lambda = 0 epsbar = 0
-    imid = int(np.ceil(1.0 / CFG["EPSSTEP"]))
-    off = epsbar[imid]
-    for i in range(imax):
-        epsbar[i] -= off
+    imid = lambdaToIndex(0)
+    epsbar -= epsbar[imid]
 
     # epsilon is the integral over epsbar
-    # TODO simplify with cumsum?
-    sum = 0.0
-    for i in range(imax):
-        sum += epsbar[i] * CFG["EPSSTEP"]
-        epsilon[i] = sum
+    epsilon = np.cumsum(epsbar * epsstep)
 
     # define the offset so that for lambda = 0 epsilon = 0
-    off = epsilon[imid]
-    for i in range(imax):
-        epsilon[i] -= off
+    epsilon -= epsilon[imid]
 
     return epsilon, epsbar, epsbarbar
