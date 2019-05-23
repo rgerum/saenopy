2de47983e741c4ee55e122ec60e9e99a4a2a30e5
Randrian <Randrian@localhost>
2019-05-23 13:39:10 +0200

    * VirtualBeads: cleaned code and added comments

--- cpp/VirtualBeads.py
+++ cpp/VirtualBeads.py
@@ -4,6 +4,8 @@ import os
 
 from numba import jit, njit
 import scipy.sparse as ssp
+from .conjugateGradient import cg
+
 
 class timeit:
 
@@ -86,34 +88,35 @@ class VirtualBeads:
         self.U_guess = U
         M._computeConnections()
 
-    def updateLocalWeigth(self, M):
-        k = 1.345
-        if self.CFG["ROBUSTMETHOD"] == "bisquare":
-            k = 4.685
-        if self.CFG["ROBUSTMETHOD"] == "cauchy":
-            k = 2.385
+    def updateLocalWeigth(self, M, method):
 
-        self.localweight = np.ones(M.N_c)
+        self.localweight[:] = 1
 
         Fvalues = np.linalg.norm(M.f_glo, axis=1)
         Fmedian = np.median(Fvalues[M.var])
 
-        if self.CFG["ROBUSTMETHOD"] == "singlepoint":
+        if method == "singlepoint":
             self.localweight[int(self.CFG["REG_FORCEPOINT"])] = 1.0e-10
 
-        if self.CFG["ROBUSTMETHOD"] == "bisquare":
+        if method == "bisquare":
+            k = 4.685
+
             index = Fvalues < k * Fmedian
             self.localweight[index * M.var] *= (1 - (Fvalues / k / Fmedian) * (Fvalues / k / Fmedian)) * (
                     1 - (Fvalues / k / Fmedian) * (Fvalues / k / Fmedian))
             self.localweight[~index * M.var] *= 1e-10
 
-        if self.CFG["ROBUSTMETHOD"] == "cauchy":
+        if method == "cauchy":
+            k = 2.385
+
             if Fmedian > 0:
                 self.localweight[M.var] *= 1.0 / (1.0 + np.power((Fvalues / k / Fmedian), 2.0))
             else:
                 self.localweight *= 1.0
 
-        if self.CFG["ROBUSTMETHOD"] == "huber":
+        if method == "huber":
+            k = 1.345
+
             index = (Fvalues > (k * Fmedian))*M.var
             self.localweight[index] = k * Fmedian / Fvalues[index]
 
@@ -122,7 +125,6 @@ class VirtualBeads:
 
         counter = np.sum(1.0 - self.localweight[M.var])
         counterall = np.sum(M.var)
-
 
         print("total weight: ", counter, "/", counterall)
 
@@ -176,220 +178,28 @@ class VirtualBeads:
         pass
 
     def computeAAndb(self, M, alpha):
-        uselaplace = self.CFG["REGMETHOD"] == "laplace"
-        print("uselaplace", uselaplace)
-        lagrain = self.CFG["REG_LAPLACEGRAIN"] ** 2
-        llambda_z = self.CFG["REG_SIGMAZ"]
+        KA = M.K_glo.multiply(np.repeat(self.localweight * alpha, 3)[None, :])
+        self.KAK = KA @ M.K_glo
+        self.A = self.I + self.KAK
 
-        import time
-        if 1:
-            with timeit("computeAAndb"):
-                #with timeit("multiply A"):
-                #    with timeit("1"):
-                AK = M.K_glo.multiply(np.repeat(self.localweight*alpha, 3)[:, None])
-                KA = M.K_glo.multiply(np.repeat(self.localweight * alpha, 3)[None, :])
-                self.KAK = M.K_glo @ AK
-                self.A = self.I + self.KAK
+        self.b = (KA @ M.f_glo.ravel()).reshape(M.f_glo.shape)
 
-                self.b = (KA @ M.f_glo.ravel()).reshape(M.f_glo.shape)
+        index = M.var * self.vbead
+        self.b[index] += self.U_found[index] - M.U[index]
 
-                index = M.var * self.vbead
-                self.b[index] += self.U_found[index] - M.U[index]
-            return
-
-        if 0:
-            K1 = M.K_glo[0:3].data.reshape(3, -1)
-            l = lwa[[0, 1, 2, 1 + 0, 1 + 1, 1 + 2, 18 + 0, 18 + 1, 18 + 2, 324 + 0, 324 + 1, 324 + 2]]
-
-        if 1:
-            #return
-            Aj = []
-            Asp = []
-            with open("outputAj.dat") as fp_Aj:
-                with open("outputAsp.dat") as fp_Asp:
-                    for l1, l2 in zip(fp_Aj, fp_Asp):
-                        Aj.append([int(i) for i in l1.split()])
-                        Asp.append(np.array([float(i) for i in l2.split()]).reshape(-1, 3, 3))
-
-            self.A_sp = []
-            self.A_j = []
-            for i in range(M.N_c):
-                self.A_sp.append([])
-                self.A_j.append([])
-            self.b = np.zeros(M.N_c)
-
-            for i in range(M.N_c):
-                if not M.var[i]:
-                    continue
-
-                if i % 100 == 0:
-                    print("computing A", int(i/M.N_c*100), "%")
-
-                self.A_j[i] = []
-                self.A_sp[i] = []
-
-                for j in self.conconnections[i]:
-                    A_ijtemp = np.zeros((3, 3))
-
-                    hasentry = False
-
-                    if M.var[j]:
-                        if uselaplace:
-                            set_intersection = self.oldconconnections[i] & self.oldconconnections[j]
-                        else:
-                            set_intersection = M.connections[i] & M.connections[j]
-
-                        for k in set_intersection:
-                            if not M.var[k]:
-                                continue
-
-                            hasentry = True
-
-                            if uselaplace:
-                                KL_ik = M.K_glo[i] @ M.Laplace[k]
-                                LK_kj = M.Laplace[k] @ M.K_glo[j]
-
-                                if M.K_glo[i].find(k) != M.K_glo[i].end():
-                                    KA_ik = M.K_glo[i][k]
-                                else:
-                                    KA_ik = np.zeros((3, 3))
-
-                                if M.K_glo[k].find(j) != M.K_glo[k].end():
-                                    AK_kj = M.K_glo[k][j]
-                                else:
-                                    AK_kj = np.zeros((3, 3))
-
-                                A_ijtemp += KA_ik*AK_kj*alpha*self.localweight[k]
-
-                                A_ijtemp += KL_ik*LK_kj*alpha*lagrain
-                            else:
-                                if 1:
-                                    KA_ik = M.K_glo[i*3:i*3+3, k*3:k*3+3].reshape(3, 3)
-                                    AK_kj = M.K_glo[k*3:k*3+3, j*3:j*3+3].reshape(3, 3)
-                                else:
-                                    KA_ik = M.K_glo_all[i, k]
-                                    AK_kj = M.K_glo_all[k, j]
-
-                                A_ijtemp += KA_ik @ AK_kj * alpha * self.localweight[k]
-                                print("i", i, "j", j, "k", k)
-                                print("KA_ik", KA_ik)
-                                print("KA_kj", AK_kj)
-                                print("alpha", alpha)
-                                print("localweight[k]", self.localweight[k])
-
-                    if i == j and self.vbead[i]:
-                        hasentry = True
-
-                        A_ijtemp[0, 0] += 1.0
-                        A_ijtemp[1, 1] += 1.0
-                        A_ijtemp[2, 2] += llambda_z
-
-                    if hasentry:
-
-                        self.A_j[i].append(j)
-                        self.A_sp[i].append(A_ijtemp)
-                    break
-                break
-
-            self.b = np.zeros((M.N_c, 3))
-            f = M.f_glo.copy()
-
-            if uselaplace:
-                lf = M.Laplace @ f
-                llf = M.Laplace @ lf
-
-            for i in range(M.N_c):
-                if M.var[i]:
-                    if i % 100 == 0:
-                        print("computing b", int(i/M.N_c*100), "%")
-
-                        if self.vbead[i]:
-                            btemp = self.U_found[i]-M.U[i]
-                            btemp[2] *= llambda_z
-                            self.b[i] += btemp
-
-                        for j in M.connections[i]:
-                            if M.var[j]:
-                                self.b[i] += M.K_glo[i][j] * f[j] * alpha * self.localweight[j]
-                                if uselaplace:
-                                    self.b[i] += M.K_glo[i][j] * llf[j] * alpha * lagrain
-
-    def getB(self):
-        M = self.M
-        llambda_z = 1
+    def recordRelaxationStatus(self, relrecname, M, relrec):
         alpha = self.CFG["ALPHA"]
 
-        b3 = np.zeros((M.N_c, 3))
-        f = M.f_glo.copy()
-
-        for i in range(M.N_c):
-            if M.var[i]:
-                if i % 100 == 0:
-                    print("computing b", int(i / M.N_c * 100), "%")
-
-                if self.vbead[i]:
-                    btemp = self.U_found[i] - M.U[i]
-                    btemp[2] *= llambda_z
-                    b3[i] += btemp
-
-                for j in M.connections[i]:
-                    if M.var[j]:
-                        b2[i] += M.K_glo[i*3:i*3+3, j*3:j*3+3] @ f[j] * alpha * self.localweight[j]
-        return b
-
-    def mulA(self, u):
-        return self.mulA_jit(u, self.M.N_c, self.A_j, self.A_sp)
-
-    @staticmethod
-    @jit()
-    def mulA_jit(u, N_c, A_j, A_sp):
-        #print("Mul A")
-        #return (self.A @ u.flatten()).reshape(u.shape)
-        f = np.zeros(u.shape)
-        for c1 in range(N_c):
-            ff = np.zeros(3)
-
-            for j, c2 in enumerate(A_j[c1]):
-
-                uu = u[3*c2:3*c2+3]
-                ff += A_sp[c1][j] @ uu
-
-            f[3*c1:3*c1+3] = ff
-        return f
-
-    def recordRelaxationStatus(self, M, relrec):
-        ff = 0.0
-        L = 0.0
-        u2 = 0.0
-        uuf2 = 0.0
-        suuf = 0.0
-
-        lagrain = self.CFG["REG_LAPLACEGRAIN"]**2
-
-        bcount = 0
-
-        alpha = self.CFG["ALPHA"]
-        sigz = self.CFG["REG_SIGMAZ"]
-
-        for b in range(M.N_c):
-            if M.var[b] and self.vbead[b]:
-                btemp = self.U_found[b] - M.U[b]
-                btemp[2] *= sigz
-                uuf2 += np.sum(btemp**2)
-                suuf += np.linalg.norm(btemp)
-                bcount += 1
+        indices = M.var & self.vbead
+        btemp = self.U_found[indices] - M.U[indices]
+        uuf2 = np.sum(btemp ** 2)
+        suuf = np.sum(np.linalg.norm(btemp, axis=1))
+        bcount = btemp.shape[0]
 
         u2 = np.sum(M.U[M.var]**2)
 
         f = np.zeros((M.N_c, 3))
         f[M.var] = M.f_glo[M.var]
-
-        if self.CFG["REGMETHOD"] == "laplace":
-            lf = M.Laplace @ f
-
-            for b in range(M.N_c):
-                if M.var[b]:
-                    f[b] += lf[b] * lagrain
 
         ff = np.sum(np.sum(f**2, axis=1)*self.localweight*M.var)
 
@@ -401,81 +211,46 @@ class VirtualBeads:
 
         relrec.append((L, uuf2, ff))
 
-        relrecname = os.path.join(self.CFG["DATAOUT"], self.CFG["REG_RELREC"])
         np.savetxt(relrecname, relrec)
 
-    def relax(self, M):
-        llambda_z = self.CFG["REG_SIGMAZ"]
-
-        #self.I = ssp.lil_matrix((3, 3))
-        #self.I.setdiag([1, 1, llambda_z])
-        #self.I = ssp.kron(np.outer(self.vbead, np.ones(M.N_c)), self.I, format="csc")
-        self.I_linear = np.repeat(self.vbead, 3)
+    def relax(self, M, stepper=0.33, REG_SOLVER_PRECISION=1e-18, i_max=100, rel_conv_crit=0.01, alpha=1.0, method="huber", relrecname=None):
         self.I = ssp.lil_matrix((self.vbead.shape[0]*3, self.vbead.shape[0]*3))
-        self.I.setdiag(self.I_linear)
+        self.I.setdiag(np.repeat(self.vbead, 3))
 
-        relrec = []
-
-        lagrain = self.CFG["REG_LAPLACEGRAIN"]**2
-
-        alpha = self.CFG["ALPHA"]
         self.M = M
 
         self.localweight = np.ones(M.N_c)
 
-        def loadK(name):
-            return
-            data = np.load(name+".npy")
-            x = np.load(name+"X.npy")
-            y = np.load(name+"Y.npy")
-            K = ssp.coo_matrix((data, (x, y)), shape=(M.N_c*3, M.N_c*3))
-            f = np.load(name+"F.npy")
-
-            self.M.K_glo = K
-            self.M.f_glo = f
-
-        def loadAb(self, name, i=0):
-            return
-            if i == 0:
-                return
-            b = np.load(name.replace("Aj", "b")+".npy")
-
-            name = name.replace("Aj", "Asp")
-            data = np.load(name+"_data.npy")
-            indices = np.load(name+"_indices.npy")
-            indptr = np.load(name+"_indptr.npy")
-            A = ssp.bsr_matrix((data, indices, indptr), shape=(b.shape[0] * 3, b.shape[0] * 3)).tocsr()
-
-            self.A = A
-            self.b = b
-
+        # update the forces on each tetrahedron and the global stiffness tensor
         print("going to update glo f and K")
         M._updateGloFAndK()
-        loadK("output/K%d" % -1)
 
-        self.recordRelaxationStatus(M, relrec)
-
-        i_max = self.CFG["REG_ITERATIONS"]
+        # log and store values (if a target file was provided)
+        if relrecname is not None:
+            relrec = []
+            self.recordRelaxationStatus(relrecname, M, relrec)
 
         print("check before relax !")
-
+        # start the iteration
         for i in range(i_max):
-            if self.CFG["REGMETHOD"] != "normal":
-                self.updateLocalWeigth(M)
-                #self.localweight = np.loadtxt("output/lw%d.dat" % i)
+            # compute the weight matrix
+            if method != "normal":
+                self.updateLocalWeigth(M, method)
 
+            # compute A and b for the linear equation that solves the regularisation problem
             self.computeAAndb(M, alpha)
-            #loadAb(self, "output/Aj%d.dat" % i, i)
 
-            uu = self._solve_CG(M)
-            #M.U = np.loadtxt("output/U%d.dat" % i)
+            # get and apply the displacements that solve the regularisation term
+            uu = self._solve_CG(M, stepper, REG_SOLVER_PRECISION)
 
+            # update the forces on each tetrahedron and the global stiffness tensor
             M._updateGloFAndK()
-            #loadK("output/K%d" % i)
 
             print("Round", i+1, " |du|=", uu)
 
-            self.recordRelaxationStatus(M, relrec)
+            # log and store values (if a target file was provided)
+            if relrecname is not None:
+                self.recordRelaxationStatus(relrecname, M, relrec)
 
             # if we have passed 6 iterations calculate average and std
             if i > 6:
@@ -485,24 +260,19 @@ class VirtualBeads:
                 Lstd = np.std(last_Ls) / np.sqrt(5)  # the original formula just had /N instead of /sqrt(N)
 
                 # if the iterations converge, stop the iteration
-                if Lstd / Lmean < self.CFG["REG_CONV_CRIT"]:
+                if Lstd / Lmean < rel_conv_crit:
                     break
-            #break
 
         return relrec
 
-    def _solve_CG(self, M):
+    def _solve_CG(self, M, stepper=0.33, REG_SOLVER_PRECISION=1e-18):
         """
         Solve the displacements from the current stiffness tensor using conjugate gradient.
         """
-        stepper = self.CFG["REG_SOLVER_STEP"]
 
         # solve the conjugate gradient which solves the equation A x = b for x
-        # where A is the stiffness matrix K_glo and b is the vector of the target forces
-        self.M = M
-        A = IA(self.KAK, self.I_linear)
-        #A.__matmul__ = self.mulA
-        uu = self.cg(self.A, self.b.flatten(), maxiter=25*int(pow(M.N_c, 0.33333)+0.5), tol=M.N_c*self.CFG["REG_SOLVER_PRECISION"]).reshape((M.N_c, 3))
+        # where A is (I - KAK) (K: stiffness matrix, A: weight matrix) and b is (u_meas - u - KAf)
+        uu = cg(self.A, self.b.flatten(), maxiter=25*int(pow(M.N_c, 0.33333)+0.5), tol=M.N_c*REG_SOLVER_PRECISION).reshape((M.N_c, 3))
 
         # add the new displacements to the stored displacements
         self.M.U += uu * stepper
@@ -511,55 +281,6 @@ class VirtualBeads:
 
         # return the total applied displacement
         return np.sqrt(du/M.N_c)
-
-    def cg(self, A, b, maxiter=1000, tol=0.00001):
-        def norm(x):
-            return np.inner(x.flatten(), x.flatten())
-
-        # calculate the total force "amplitude"
-        normb = norm(b)
-
-        # if it is not 0 (always has to be positive)
-        if normb == 0:
-            return 0
-
-        x = np.zeros_like(b)
-
-        # the difference between the desired force deviations and the current force deviations
-        r = b - A @ x
-
-        # and store it also in pp
-        p = r
-
-        # calculate the total force deviation "amplitude"
-        resid = norm(p)
-
-        # iterate maxiter iterations
-        for i in range(1, maxiter + 1):
-            Ap = A @ p
-
-            alpha = resid / np.sum(p * Ap)
-
-            x = x + alpha * p
-            r = r - alpha * Ap
-
-            rsnew = norm(r)
-
-            # check if we are already below the convergence tolerance
-            if rsnew < tol * normb:
-                break
-
-            beta = rsnew / resid
-
-            # update pp and resid
-            p = r + beta * p
-            resid = rsnew
-
-            # print status every 100 frames
-            if i % 100 == 0:
-                print(i, ":", resid, "alpha=", alpha, "du=", np.sum(x ** 2))  # , end="\r")
-
-        return x
 
     def storeUfound(self, Uname, Sname):
         np.savetxt(Uname, self.U_found)
