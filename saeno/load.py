ecf1217ca79eba9a4d1bd4d14d0be3d2fe10eae9
Randrian <Randrian@localhost>
2019-08-07 09:46:27 +0200

    load.py: improved loading of gmsh files

--- saeno/load.py
+++ saeno/load.py
@@ -10,22 +10,40 @@ def load_gmsh(filename):
     def read_node_block(file_iter):
         line = next(file_iter)
         entityDim, entityTag, parametric, numNodesInBlock = line.split()
+        first_index = 0
+        last_index = 1
+        indices = np.zeros(int(numNodesInBlock), dtype=int)
         for i in range(int(numNodesInBlock)):
             line = next(file_iter)
+            if i == 0:
+                first_index = int(line)
+            if i == int(numNodesInBlock)-1:
+                last_index = int(line)+1
+            indices[i] = int(line)
             pass
-        data = np.zeros((int(numNodesInBlock), int(entityDim)))
+        if file_iter.nodes is None:
+            file_iter.nodes = np.zeros((int(last_index), 3))
+        if file_iter.nodes.shape[0] < last_index:
+            file_iter.nodes = np.concatenate((file_iter.nodes, np.zeros((last_index-file_iter.nodes.shape[0], 3))))
+        data = np.zeros((int(numNodesInBlock), 3))
         for i in range(int(numNodesInBlock)):
             line = next(file_iter)
-            data[i, :] = [float(s) for s in line.split()]
-        return entityTag, data
+            if 1:#entityDim != "0" and entityDim != "1" and en:
+                data[i, :] = [float(s) for s in line.split()]
+        file_iter.nodes[indices, :] = data
+        return entityDim+"_"+entityTag, data, first_index
 
     def read_nodes(file_iter):
         if version_major == "4":
-            numEntityBlocks, numNodes, minNodeTag, maxNodeTag = next(file_iter).split()
+            line = next(file_iter).split()
+            try:
+                numEntityBlocks, numNodes, minNodeTag, maxNodeTag = line
+            except ValueError:
+                numEntityBlocks, numNodes = line
             for i in range(int(numEntityBlocks)):
-                tag, data = read_node_block(file_iter)
-                #node_blocks[tag] = data
-                entities[int(tag)]["nodes"] = data
+                tag, data, first_index = read_node_block(file_iter)
+                entities[tag]["nodes"] = data
+                entities[tag]["first_index"] = first_index
         if version_major == "2":
             global nodes
             numNodes = int(next(file_iter))
@@ -39,22 +57,28 @@ def load_gmsh(filename):
     def read_elements_block(file_iter):
         line = next(file_iter)
         entityDim, entityTag, elementType, numElementsInBlock = line.split()
-        if elementType == "4":
-            data = np.zeros((int(numElementsInBlock), 4))
+        nodes_per_type = {"1": 2, "2": 3, "3": 4, "4":4, "5":8, "6":6, "7":5, "8":3, "9":6, "10":9, "11":10, "12":27, "13":18, "14":14, "15": 1}
+        if elementType in nodes_per_type.keys():
+            data = np.zeros((int(numElementsInBlock), nodes_per_type[elementType]), dtype=int)
             for i in range(int(numElementsInBlock)):
                 line = next(file_iter)
+                #if elementType == "5":
+                #    print("e", line)
                 data[i, :] = [int(s) for s in line.split()[1:]]
         else:
+            for i in range(int(numElementsInBlock)):
+                line = next(file_iter)
             data = None
-        return entityTag, data
+        return entityDim+"_"+entityTag, data
 
     def read_elements(file_iter):
         if version_major == "4":
             numEntityBlocks, numNodes, minNodeTag, maxNodeTag = next(file_iter).split()
             for i in range(int(numEntityBlocks)):
                 tag, data = read_elements_block(file_iter)
-                #elements_blocks[tag] = data
-                entities[int(tag)]["elements"] = data
+                #if entities[tag]["first_index"]:
+                #    data -= entities[tag]["first_index"]+1
+                entities[tag]["elements"] = data
         if version_major == "2":
             numElements = int(next(file_iter))
             data = np.zeros((int(numElements), 4))
@@ -68,10 +92,16 @@ def load_gmsh(filename):
         numPoints, numCurves, numSurfaces, numVolumes = next(file_iter).split()
         for i in range(int(numPoints)):
             line = next(file_iter)
+            tag, _ = line.split(maxsplit=1)
+            entities["0_"+tag] = dict(type="point")
         for i in range(int(numCurves)):
             line = next(file_iter)
+            tag, _ = line.split(maxsplit=1)
+            entities["1_"+tag] = dict(type="curve")
         for i in range(int(numSurfaces)):
             line = next(file_iter)
+            tag, _ = line.split(maxsplit=1)
+            entities["2_"+tag] = dict(type="surface")
         for i in range(int(numVolumes)):
             line = next(file_iter)
             data = line.split()
@@ -84,7 +114,7 @@ def load_gmsh(filename):
             surf_tags = []
             for t in data[7+numPhysicalTags:7+numPhysicalTags+numBoundngSurfaces]:
                 surf_tags.append(int(t))
-            entities[int(volumeTag)] = dict(type="volume", minX=float(minX), minY=float(maxY), minZ=float(minZ),
+            entities["3_"+volumeTag] = dict(type="volume", minX=float(minX), minY=float(maxY), minZ=float(minZ),
                                                            maxX=float(maxX), maxY=float(maxY), maxZ=float(maxZ),
                                                            physical_tags=phys_tags, surface_tags=surf_tags)
 
@@ -95,7 +125,11 @@ def load_gmsh(filename):
             if line.startswith(b"$MeshFormat"):
                 line = next(file_iter)
                 version, file_type, data_size = line.split()
-                version_major, version_minor = version.split(b".")
+                try:
+                    version_major, version_minor = version.split(b".")
+                except ValueError:
+                    version_major = version
+                    version_minor = "0"
                 if int(file_type) == 1:
                     raise IOError("Gmesh reader does not support binary format")
                 if int(version_major) != 4 and int(version_major) != 2:
@@ -103,25 +137,41 @@ def load_gmsh(filename):
 
     with open(filename, "r") as fp:
         file_iter = iter(fp)
+        file_iter.nodes = None
         for line in file_iter:
             if line.startswith("$MeshFormat"):
                 line = next(file_iter)
                 version, file_type, data_size = line.split()
-                version_major, version_minor = version.split(".")
+                try:
+                    version_major, version_minor = version.split(".")
+                except ValueError:
+                    version_major = version
+                    version_minor = "0"
             if line.startswith("$Entities"):
                 read_entities(file_iter)
             if line.startswith("$Nodes"):
                 nodes = read_nodes(file_iter)
             if line.startswith("$Elements"):
                 tetrahedra = read_elements(file_iter)
+        nodes = file_iter.nodes
 
     for entityId in entities:
         entity = entities[entityId]
         if entity["type"] == "volume":
-            nodes = entity["nodes"]
+            #nodes = entity["nodes"]
             tetrahedra = entity["elements"]
+            #print("volumne", tetrahedra, entity)
 
-    M = FiniteBodyForces()
-    M.setNodes(nodes)
-    M.setTetrahedra(tetrahedra-1)
+    if tetrahedra.shape[1] == 4:
+        M = FiniteBodyForces()
+        M.setNodes(nodes)
+        M.setTetrahedra(tetrahedra-1)
+    else:
+        from .FiniteBodyForcesHex import FiniteBodyForces
+        M = FiniteBodyForces()
+        unique_indices = np.unique(tetrahedra)
+        index_lookup = np.ones(max([tetrahedra.max()+1, unique_indices.max()+1]), dtype=int)*np.nan
+        index_lookup[unique_indices] = np.arange(len(unique_indices), dtype=int)
+        M.setNodes(nodes[unique_indices, :])
+        M.setHexahedra(index_lookup[tetrahedra])
     return M
