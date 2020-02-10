be532685267fd5881710cc96333389d1f18dd86c
Richard Gerum <14153051+rgerum@users.noreply.github.com>
2020-02-10 10:30:55 +0100

    macro.py added getQuadrature and combineQuadrature

--- saenopy/macro.py
+++ saenopy/macro.py
@@ -1,8 +1,95 @@
 import numpy as np
 from .buildBeams import buildBeams
+from .materials import Material
+from typing import Sequence
 
 
-def getShearRheometerStress(gamma, material, s=None):
+def getQuadrature(N: int, xmin: float, xmax: float) -> (np.ndarray, np.ndarray):
+    """
+    Provides N quadrature points for an integration from xmin to xmax together with their weights.
+
+    Parameters
+    ----------
+    N : int
+        The number of quadrature points to use. Has to be 1 <= N <= 5.
+    xmin : float
+        The start of the integration range
+    xmax : float
+        The end of the integration range
+
+    Returns
+    -------
+    points : np.ndarray
+        The points of the quadrature
+    w : np.ndarray
+        The weights of the points
+    """
+    if N < 1:
+        raise ValueError()
+
+    if N == 1:
+        points = [0]
+        w = [2]
+
+    if N == 2:
+        points = [-np.sqrt(1 / 3), np.sqrt(1 / 3)]
+        w = [1, 1]
+
+    if N == 3:
+        points = [-np.sqrt(3 / 5), 0, np.sqrt(3 / 5)]
+        w = [5 / 9, 8 / 9, 5 / 9]
+
+    if N == 4:
+        points = [-np.sqrt(3 / 7 - 2 / 7 * np.sqrt(6 / 5)), +np.sqrt(3 / 7 - 2 / 7 * np.sqrt(6 / 5)),
+                  -np.sqrt(3 / 7 + 2 / 7 * np.sqrt(6 / 5)), +np.sqrt(3 / 7 + 2 / 7 * np.sqrt(6 / 5))]
+        w = [(18 + np.sqrt(30)) / 36, (18 + np.sqrt(30)) / 36, (18 - np.sqrt(30)) / 36, (18 - np.sqrt(30)) / 36]
+
+    if N == 5:
+        points = [0,
+                  -1 / 3 * np.sqrt(5 - 2 * np.sqrt(10 / 7)), +1 / 3 * np.sqrt(5 - 2 * np.sqrt(10 / 7)),
+                  -1 / 3 * np.sqrt(5 + 2 * np.sqrt(10 / 7)), +1 / 3 * np.sqrt(5 + 2 * np.sqrt(10 / 7))]
+        w = [128 / 225, (322 + 13 * np.sqrt(70)) / 900, (322 + 13 * np.sqrt(70)) / 900, (322 - 13 * np.sqrt(70)) / 900,
+             (322 - 13 * np.sqrt(70)) / 900]
+
+    if N > 5:
+        raise ValueError()
+
+    points = np.array(points)
+    w = np.array(w)
+    factor = (xmax - xmin) / 2
+    points = factor * points + (xmax + xmin) / 2
+    w = w * factor
+    return points, w
+
+
+def combineQuadrature(p1_w1: Sequence, p2_w2: Sequence) -> (np.ndarray, np.ndarray, np.ndarray):
+    """
+    Combine the quadratures of two different axes.
+
+    Parameters
+    ----------
+    p1_w1 : tuple
+        the points and weights for the first axis
+    p2_w2 : tuple
+        the points and weights for the second axis
+
+    Returns
+    -------
+    x : np.ndarray
+        the points for the first axis
+    y : np.ndarray
+        the points for the second axis
+    w : np.ndarray
+        the combined weights for the points
+    """
+    p1, w1 = p1_w1
+    p2, w2 = p2_w2
+    x, y = [f.ravel() for f in np.meshgrid(p1, p2)]
+    w = (w1[:, None] * w2[None, :]).ravel()
+    return x, y, w
+
+
+def getShearRheometerStress(gamma: np.ndarray, material: Material, s: np.ndarray = None) -> (np.ndarray, np.ndarray):
     r"""
     Get the stress for a given strain of the material in a shear rheometer.
 
@@ -51,6 +138,34 @@ def getShearRheometerStress(gamma, material, s=None):
 
     W = np.mean(eps, axis=-1)
     dW = np.diff(W) / np.diff(gamma)
+    return gamma[:-1] + np.diff(gamma) / 2, dW
+
+
+def getShearRheometerStressRotation(gamma, material, H=1e-3, R=10e-3, s=30, q=2):
+    if isinstance(s, int):
+        s = buildBeams(s)
+
+    x_r, z_h, w = combineQuadrature(getQuadrature(q, 0, 1), getQuadrature(q, 0, 1))
+
+    F = np.zeros((gamma.shape[0], len(z_h), 3, 3))
+    theta = gamma * H / R
+    theta_p = theta[:, None] * z_h[None, :]
+
+    cos, sin = np.cos(theta_p), np.sin(theta_p)
+    xtheta_h = x_r * theta[:, None] * R / H
+    F[:, :, 0, 0], F[:, :, 0, 1], F[:, :, 0, 2] = cos, -sin, -sin * xtheta_h
+    F[:, :, 1, 0], F[:, :, 1, 1], F[:, :, 1, 2] = sin, cos, cos * xtheta_h
+    F[:, :, 2, 2] = 1
+
+    s_bar = F @ s.T
+
+    s_abs = np.linalg.norm(s_bar, axis=-2)
+    eps = material.energy(s_abs - 1)
+
+    W = np.mean(eps, axis=-1)
+    W = np.average(W, axis=-1, weights=w)
+    dW = np.diff(W) / np.diff(gamma)
+
     return gamma[:-1] + np.diff(gamma) / 2, dW
 
 
