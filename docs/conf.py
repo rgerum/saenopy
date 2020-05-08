df6938b41b39d60f56d7103b3d2a5292f26c8f8d
Richard Gerum <richard.gerum+dev@protonmail.com>
2020-05-08 16:03:27 +0200

    docs: conf.py import saenopy folder

--- docs/conf.py
+++ docs/conf.py
@@ -21,6 +21,12 @@ import sys
 import os
 import shlex
 
+print(os.getcwd())
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 import mock
 # try to import the modules of the package and mock everything that is not found
