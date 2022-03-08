70f91b60181f5dfba80a05dbc4c816a58d9c8b0b
rgerum <14153051+rgerum@users.noreply.github.com>
2022-03-08 14:26:48 -0500

    added joint forces to setup.py

--- setup.py
+++ setup.py
@@ -36,6 +36,7 @@ setup(name='saenopy',
           'pyvista',
           'pyvistaqt>=0.6.0',
           'imagecodecs',
+          'jointforces @ https://github.com/christophmark/jointforces/archive/master.zip'
       ],
       extras_require={
         'compilation':  ["numba"],
