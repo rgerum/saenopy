2939ecc8ed0b9b2b2840654805409373d0a51a40
Richard Gerum <richard.gerum+dev@protonmail.com>
2022-04-04 17:41:22 +0200

    set version to v0.7.4

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@
 from setuptools import setup
 
 setup(name='saenopy',
-      version="0.7.3",
+      version="0.7.4",
       description='Semi-elastic fiber optimisation in python.',
       author='Richard Gerum',
       author_email='richard.gerum@fau.de',
