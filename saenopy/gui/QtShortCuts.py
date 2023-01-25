add scaling option for the arrows (fixes #7)

saenopy/gui/QtShortCuts.py
@@ -135,7 +135,7 @@ class QInputNumber(QInput):
     slider_dragged = False
 
     def __init__(self, layout=None, name=None, value=0, min=None, max=None, use_slider=False, float=True, decimals=2,
-                 unit=None, step=None, name_post=None, **kwargs):
+                 unit=None, step=None, name_post=None, log_slider=False, **kwargs):
         # initialize the super widget
         QInput.__init__(self, layout, name, **kwargs)
 
@@ -154,8 +154,13 @@ class QInputNumber(QInput):
             # slider
             self.slider = QtWidgets.QSlider(QtCore.Qt.Horizontal)
             self.layout().addWidget(self.slider)
-            self.slider.setRange(min * self.decimal_factor, max * self.decimal_factor)
-            self.slider.valueChanged.connect(lambda x: self._valueChangedEvent(x / self.decimal_factor))
+            self.log_slider = log_slider
+            if log_slider:
+                self.slider.setRange(int(np.log10(min) * self.decimal_factor), int(np.log10(max) * self.decimal_factor))
+                self.slider.valueChanged.connect(lambda x: self._valueChangedEvent(10**(x / self.decimal_factor)))
+            else:
+                self.slider.setRange(int(min * self.decimal_factor), int(max * self.decimal_factor))
+                self.slider.valueChanged.connect(lambda x: self._valueChangedEvent(x / self.decimal_factor))
             self.slider.sliderPressed.connect(lambda: self._setSliderDragged(True))
             self.slider.sliderReleased.connect(lambda: self._setSliderDragged(False))
         else:
@@ -204,7 +209,10 @@ class QInputNumber(QInput):
     def _doSetValue(self, value):
         self.spin_box.setValue(value)
         if self.slider is not None:
-            self.slider.setValue(value * self.decimal_factor)
+            if self.log_slider:
+                self.slider.setValue(int(np.log10(value) * self.decimal_factor))
+            else:
+                self.slider.setValue(int(value * self.decimal_factor))
 
     def value(self):
         return self.spin_box.value()
@@ -936,3 +944,171 @@ for index, (color, sat, val) in enumerate(zip(colors, saturations, value)):
         QtWidgets.QColorDialog.setStandardColor(index, QtGui.QColor(color_integer))  # for Qt5
     except TypeError:
         QtWidgets.QColorDialog.setStandardColor(index, color_integer)  # for Qt4
+
+
+import matplotlib.pyplot as plt
+class ColorMapChoose(QtWidgets.QDialog):
+    """ A dialog to select a colormap """
+    result = ""
+
+    def __init__(self, parent: QtWidgets.QWidget, map):
+        """ initialize the dialog with all the colormap of matplotlib """
+        QtWidgets.QDialog.__init__(self, parent)
+        main_layout = QtWidgets.QVBoxLayout(self)
+        self.layout = QtWidgets.QHBoxLayout()
+        main_layout.addLayout(self.layout)
+        button_layout = QtWidgets.QHBoxLayout()
+        main_layout.addLayout(button_layout)
+        self.button_cancel = QtWidgets.QPushButton("Cancel")
+        self.button_cancel.clicked.connect(lambda _: self.done(0))
+        button_layout.addStretch()
+        button_layout.addWidget(self.button_cancel)
+
+        self.maps = plt.colormaps()
+        self.buttons = []
+        self.setWindowTitle("Select colormap")
+
+        # Have colormaps separated into categories:
+        # http://matplotlib.org/examples/color/colormaps_reference.html
+        cmaps = [('Perceptually Uniform Sequential', [
+            'viridis', 'plasma', 'inferno', 'magma']),
+                 ('Sequential', [
+                     'Greys', 'Purples', 'Blues', 'Greens', 'Oranges', 'Reds',
+                     'YlOrBr', 'YlOrRd', 'OrRd', 'PuRd', 'RdPu', 'BuPu',
+                     'GnBu', 'PuBu', 'YlGnBu', 'PuBuGn', 'BuGn', 'YlGn']),
+                 ('Sequential (2)', [
+                     'binary', 'gist_yarg', 'gist_gray', 'gray', 'bone', 'pink',
+                     'spring', 'summer', 'autumn', 'winter', 'cool', 'Wistia',
+                     'hot', 'afmhot', 'gist_heat', 'copper']),
+                 ('Diverging', [
+                     'PiYG', 'PRGn', 'BrBG', 'PuOr', 'RdGy', 'RdBu',
+                     'RdYlBu', 'RdYlGn', 'Spectral', 'coolwarm', 'bwr', 'seismic']),
+                 ('Qualitative', [
+                     'Pastel1', 'Pastel2', 'Paired', 'Accent',
+                     'Dark2', 'Set1', 'Set2', 'Set3',
+                     'tab10', 'tab20', 'tab20b', 'tab20c']),
+                 ('Miscellaneous', [
+                     'turbo', 'flag', 'prism', 'ocean', 'gist_earth', 'terrain', 'gist_stern',
+                     'gnuplot', 'gnuplot2', 'CMRmap', 'cubehelix', 'brg', 'hsv',
+                     'gist_rainbow', 'rainbow', 'nipy_spectral', 'gist_ncar'])]
+
+        for cmap_category, cmap_list in cmaps:
+            layout = QtWidgets.QVBoxLayout()
+            label = QtWidgets.QLabel(cmap_category)
+            layout.addWidget(label)
+            label.setFixedWidth(150)
+            for cmap in cmap_list:
+                button = QtWidgets.QPushButton(cmap)
+                button.setStyleSheet("text-align: center; border: 2px solid black; "+self.getBackground(cmap))
+                button.clicked.connect(lambda _, cmap=cmap: self.buttonClicked(cmap))
+                self.buttons.append(button)
+                layout.addWidget(button)
+            layout.addStretch()
+            self.layout.addLayout(layout)
+
+    def buttonClicked(self, text: str):
+        """ the used as selected a colormap, we are done """
+        self.result = text
+        self.done(1)
+
+    def exec(self):
+        """ execute the dialog and return the result """
+        result = QtWidgets.QDialog.exec(self)
+        return self.result, result == 1
+
+    def getBackground(self, color: str) -> str:
+        """ convert a colormap to a gradient background """
+        import matplotlib.pyplot as plt
+        import matplotlib as mpl
+        try:
+            cmap = plt.get_cmap(color)
+        except:
+            return ""
+        text = "background-color: qlineargradient(x1: 0, y1: 0, x2: 1, y2: 0, "
+        N = 10
+        for i in range(N):
+            i = i / (N - 1)
+            text += f"stop: {i:.2f} {mpl.colors.to_hex(cmap(i))}, "
+        text = text[:-2] + ");"
+        return text
+
+
+class QDragableColor(QtWidgets.QLabel):
+    """ a color widget that can be dragged onto another QDragableColor widget to exchange the two colors.
+    Alternatively it can be right-clicked to select either a color or a colormap through their respective menus.
+    The button can represent either a single color or a colormap.
+    """
+
+    color_changed = QtCore.Signal(str)
+    color_changed_by_color_picker = QtCore.Signal(bool)
+    valueChanged = QtCore.Signal(str)
+
+    def __init__(self, value: str):
+        """ initialize with a color """
+        super().__init__(value)
+        import matplotlib.pyplot as plt
+        self.maps = plt.colormaps()
+        self.setAlignment(QtCore.Qt.AlignHCenter)
+        self.setColor(value, True)
+
+    def getBackground(self) -> str:
+        """ get the background of the color button """
+
+        try:
+            cmap = plt.get_cmap(self.color)
+        except:
+            return ""
+        text = "background-color: qlineargradient(x1: 0, y1: 0, x2: 1, y2: 0, "
+        N = 10
+        for i in range(N):
+            i = i / (N - 1)
+            text += f"stop: {i:.2f} {mpl.colors.to_hex(cmap(i))}, "
+        text = text[:-2] + ");"
+        return text
+
+    def setColor(self, value: str, no_signal=False):
+        """ set the current color """
+        # display and save the new color
+        self.color = value
+        self.setText(value)
+        self.color_changed.emit(value)
+        self.valueChanged.emit(value)
+        if value in self.maps:
+            self.setStyleSheet("text-align: center; border: 2px solid black; padding: 0.1em; "+self.getBackground())
+        else:
+            self.setStyleSheet(f"text-align: center; background-color: {value}; border: 2px solid black; padding: 0.1em; ")
+
+    def getColor(self) -> str:
+        """ get the current color """
+        # return the color
+        return self.color
+
+    def value(self):
+        return self.color
+
+    def setValue(self, value):
+        self.setColor(value)
+
+    def mousePressEvent(self, event):
+        """ when a mouse button is pressed """
+        # a mouse button opens a color choose menu
+        if event.button() == QtCore.Qt.LeftButton:
+            self.openDialog()
+
+    def openDialog(self):
+        """ open a color chooser dialog """
+        if self.color in self.maps:
+            dialog = ColorMapChoose(self.parent(), self.color)
+            colormap, selected = dialog.exec()
+            if selected is False:
+                return
+            self.setColor(colormap)
+        else:
+            # get new color from color picker
+            qcolor = QtGui.QColor(*tuple(int(x * 255) for x in mpl.colors.to_rgb(self.getColor())))
+            color = QtWidgets.QColorDialog.getColor(qcolor, self.parent())
+            # if a color is set, apply it
+            if color.isValid():
+                color = "#%02x%02x%02x" % color.getRgb()[:3]
+                self.setColor(color)
+                self.color_changed_by_color_picker.emit(True)

saenopy/gui_solver/Regularizer.py
@@ -190,7 +190,12 @@ class Regularizer(PipelineModule):
             if self.vtk_toolbar.use_center.value() is True:
                 center = M.getCenter(mode="Force")
             display_image = getVectorFieldImage(self)
-            showVectorField(self.plotter, M, -M.f * M.reg_mask[:, None], "f", center=center, factor=0.15, scalebar_max=self.vtk_toolbar.getScaleMax(), show_nan=self.vtk_toolbar.use_nans.value(), display_image=display_image, show_grid=self.vtk_toolbar.show_grid.value(), stack_shape=np.array(self.result.stack[0].shape[:3])*np.array(self.result.stack[0].voxel_size))
+            showVectorField(self.plotter, M, -M.f * M.reg_mask[:, None], "f", center=center,
+                            factor=0.15 * self.vtk_toolbar.arrow_scale.value(),
+                            colormap=self.vtk_toolbar.colormap_chooser.value(),
+                            scalebar_max=self.vtk_toolbar.getScaleMax(), show_nan=self.vtk_toolbar.use_nans.value(),
+                            display_image=display_image, show_grid=self.vtk_toolbar.show_grid.value(),
+                            stack_shape=np.array(self.result.stack[0].shape[:3])*np.array(self.result.stack[0].voxel_size))
             if cam_pos is not None:
                 self.plotter.camera_position = cam_pos
             relrec = getattr(self.result.solver[self.t_slider.value()], "relrec", None)

saenopy/gui_solver/showVectorField.py
@@ -46,11 +46,14 @@ def showVectorField2(self, M, points_name):
     showVectorField(self.plotter, M, field, points_name,
                     scalebar_max=self.vtk_toolbar.getScaleMax(), show_nan=self.vtk_toolbar.use_nans.value(),
                     display_image=display_image, show_grid=self.vtk_toolbar.show_grid.value(),
+                    factor=0.1*self.vtk_toolbar.arrow_scale.value(),
+                    colormap=self.vtk_toolbar.colormap_chooser.value(),
                     stack_shape=np.array(self.result.stack[0].shape[:3])*np.array(self.result.stack[0].voxel_size))
 
 
 def showVectorField(plotter: QtInteractor, obj: Solver, field: np.ndarray, name: str, center=None, show_nan=True, stack_shape=None,
-                    show_all_points=False, factor=.1, scalebar_max=None, display_image=None, show_grid=True):
+                    show_all_points=False, factor=.1, scalebar_max=None, display_image=None, show_grid=True,
+                    colormap="turbo"):
     # ensure that the image is either with color channels or no channels
     if (display_image is not None) and (display_image[0].shape[2] == 1):
         display_image[0] = display_image[0][:, :, 0]
@@ -114,14 +117,14 @@ def showVectorField(plotter: QtInteractor, obj: Solver, field: np.ndarray, name:
 
         # show the points
         if show_all_points:
-            plotter.add_mesh(point_cloud, colormap="turbo", scalars=name + "_mag2", render=False)
+            plotter.add_mesh(point_cloud, colormap=colormap, scalars=name + "_mag2", render=False)
         elif show_nan:
             if R.shape[0]:
-                plotter.nan_actor = plotter.add_mesh(point_cloud2, colormap="turbo", scalars="nan",
+                plotter.nan_actor = plotter.add_mesh(point_cloud2, colormap=colormap, scalars="nan",
                                                      show_scalar_bar=False, render=False)
 
         # add the arrows
-        plotter.add_mesh(arrows, scalar_bar_args=sargs, colormap="turbo", name="arrows", render=False)
+        plotter.add_mesh(arrows, scalar_bar_args=sargs, colormap=colormap, name="arrows", render=False)
 
         # update the scalebar
         plotter.auto_value = np.nanpercentile(point_cloud[name + "_mag2"], 99.9)