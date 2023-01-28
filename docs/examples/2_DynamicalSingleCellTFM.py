879cc6f4ad6f2fafddcef842d3a9c41cf76d4c32
David <david.boehringer@fau.de>
2023-01-28 18:09:37 +0100

    Add paths relative to the SOURCE directory now

--- docs/examples/2_DynamicalSingleCellTFM.py
+++ docs/examples/2_DynamicalSingleCellTFM.py
@@ -6,9 +6,7 @@ This example evaluates a single natural killer cell that migrated through 1.2mg/
 
 
 
- .. figure:: img_example/Gif_nk_dynamic_example.*
-
-
+ .. figure:: ../examples/img_example/Gif_nk_dynamic_example.*
 
 
 This example can also be evaluated with the graphical user interface.
