3eba5d7c7aba7e9933126bd5816eba0ca46c5436
David <david.boehringer@fau.de>
2023-01-29 14:06:28 +0100

    update bead example

--- docs/source/examples/3_BeadMeasurement.py
+++ docs/source/examples/3_BeadMeasurement.py
@@ -2,8 +2,13 @@
 Microbeads to Detect Matrix Deformations
 =======================
 This example calculates matrix deformations around a Human Umbilical Vein Endothelial Cells (HUVECs) that invades in a polyethylene glycol 841
-(PEG) hydrogel cell between a relaxed and deformed stacks as measured and described in `[Jorge Barrasa-Fano et al., 2021] <https://www.sciencedirect.com/science/article/pii/S2352711021000625>`_.                                                
-The data is available from the authors website here `here <https://www.mech.kuleuven.be/en/bme/research/mechbio/lab/tfmlab>`_.
+(PEG) hydrogel cell  as measured and described in `[Jorge Barrasa-Fano et al., 2021] <https://www.sciencedirect.com/science/article/pii/S2352711021000625>`_.                                                
+The data is available from the authors website `here <https://www.mech.kuleuven.be/en/bme/research/mechbio/lab/tfmlab>`_.
+Here images of microbeads (channel 'ch01') between a relaxed and deformed stacks are used to calculate matrix deformations (image on the right).
+
+
+ .. figure:: ../images/Microbeads.png''
+
 
 """
 
@@ -18,19 +23,19 @@ import saenopy
 # ------------------
 #
 # Saenopy is very flexible in loading stacks from any filename structure.
-# Here we replace the number in the position "Pos004" with an asterisk "Pos*" to batch process all positions.
-# We replace the number of the channels "ch00" with a channel placeholder "ch{c:00}" to indicate that this refers to
+# We replace the number of the channels "ch00" with a channel placeholder "ch{c:01}" to indicate that this refers to
 # the channels and which channel to use as the first channel where the deformations should be detected.
 # We replace the number of the z slice "z000" with a z placeholder "z{z}" to indicate that this number refers to the
 # z slice. We do the same for the deformed state and for the reference stack.
+# Here images of microbeads (channel 'ch01') are used to calculate matrix deformation.
 
 # load the relaxed and the contracted stack
 # {z} is the placeholder for the z stack
 # {c} is the placeholder for the channels
 # {t} is the placeholder for the time points
 results = saenopy.get_stacks( 
-    'TestDataTFMlabKULeuven\Stressed_z{z}_ch{c:00}.tif',
-    reference_stack='TestDataTFMlabKULeuven\Relaxed_z{z}_ch{c:00}.tif',
+    'TestDataTFMlabKULeuven\Stressed_z{z}_ch{c:01}.tif',
+    reference_stack='TestDataTFMlabKULeuven\Relaxed_z{z}_ch{c:01}.tif',
     output_path='3_BeadMeasurement/example_output',
     voxel_size=[0.567, 0.567, 0.493])
 
@@ -38,8 +43,7 @@ results = saenopy.get_stacks(
 # Detecting the Deformations
 # --------------------------
 # Saenopy uses 3D Particle Image Velocimetry (PIV) with the following parameters 
-# to calculate matrix deformations between a deformed and relaxed state 
-# for three example cells.
+# to calculate matrix deformations between a deformed and relaxed state.  
 #
 # +------------------+-------+
 # | Piv Parameter    | Value |
