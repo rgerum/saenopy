d4f1c7457bb8be6f27fb3e30409ccf664b72e125
rgerum <14153051+rgerum@users.noreply.github.com>
2023-05-04 12:40:02 -0400

    fixed exporting stack images

--- saenopy/gui/solver/modules/StackDisplay.py
+++ saenopy/gui/solver/modules/StackDisplay.py
@@ -172,8 +172,12 @@ class StackDisplay(PipelineModule):
                 stack1, stack2 = self.result.stack_reference[:, :, :, z, c], self.result.stack[t][:, :, :, z, c]
             else:
                 stack1, stack2 = self.result.stack[t][:, :, :, z, c], self.result.stack[t + 1][:, :, :, z, c]
-            tifffile.imsave(new_path.parent / (new_path.stem + "_relaxed.tif"), stack1)
-            tifffile.imsave(new_path.parent / (new_path.stem + "_deformed.tif"), stack2)
+            tifffile.imwrite(new_path.parent / (new_path.stem + "_relaxed.tif"), stack1)
+            tifffile.imwrite(new_path.parent / (new_path.stem + "_deformed.tif"), stack2)
+            if len(stack1.shape) == 3 and stack1.shape[2] == 1:
+                stack1 = stack1[:, :, 0]
+            if len(stack2.shape) == 3 and stack2.shape[2] == 1:
+                stack2 = stack2[:, :, 0]
             imageio.mimsave(new_path.parent / (new_path.stem + ".gif"), [stack1, stack2], duration=1000 * 1./2)
 
     def update_display(self):
