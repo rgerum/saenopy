939a80913be67c9d10c030be3e6be675d9dd7e9d
Richard Gerum <14153051+rgerum@users.noreply.github.com>
2023-05-19 15:30:04 -0400

    added test for mesh to raise errors when attributes set with invalid shape

---
+++ test/test_mesh.py
@@ -0,0 +1,35 @@
+from saenopy.solver import SolverMesh
+from saenopy.multigrid_helper import get_scaled_mesh
+import numpy as np
+from valid8.entry_points import ValidationError
+import pytest
+
+
+def test_mesh_shape():
+    nodes, tetrahedra = get_scaled_mesh(3, 10, 10, [5, 5, 5], 1)
+
+    mesh = SolverMesh()
+    mesh.energy = np.random.rand(tetrahedra.shape[0] + 1)
+    mesh.displacements = np.random.rand(tetrahedra.shape[0] + 1)
+    mesh.regularisation_mask = np.random.rand(tetrahedra.shape[0] + 1)
+
+    mesh = SolverMesh(nodes, tetrahedra)
+    mesh.energy = np.random.rand(tetrahedra.shape[0])
+    with pytest.raises(ValidationError, match="InvalidShape"):
+        mesh.energy = np.random.rand(tetrahedra.shape[0]+1)
+    with pytest.raises(ValidationError, match="InvalidShape"):
+        mesh.energy = np.random.rand(tetrahedra.shape[0], 10)
+
+    mesh.displacements = np.random.rand(nodes.shape[0], 3)
+    with pytest.raises(ValidationError, match="InvalidShape"):
+        mesh.displacements = np.random.rand(nodes.shape[0], 4)
+    with pytest.raises(ValidationError, match="InvalidShape"):
+        mesh.displacements = np.random.rand(nodes.shape[0]+1, 3)
+    with pytest.raises(ValidationError, match="InvalidShape"):
+        mesh.displacements = np.random.rand(nodes.shape[0])
+
+    mesh.regularisation_mask = np.random.rand(nodes.shape[0])
+    with pytest.raises(ValidationError, match="InvalidShape"):
+        mesh.regularisation_mask = np.random.rand(nodes.shape[0]+1)
+    with pytest.raises(ValidationError, match="InvalidShape"):
+        mesh.regularisation_mask = np.random.rand(nodes.shape[0], 3)
